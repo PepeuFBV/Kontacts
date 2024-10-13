7c8902da45b5b88741eab6d5eb80da39bb574ecc
Pedro <pedrofigueiraboaviagem@gmail.com>
2024-10-13 15:18:57 -0300

    add contact screen done

--- app/src/main/java/com/example/kontacts/MainActivity.kt
+++ app/src/main/java/com/example/kontacts/MainActivity.kt
@@ -1,14 +1,52 @@
 package com.example.kontacts
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
 import androidx.room.Room
+import com.example.kontacts.ui.theme.PurpleMain
 import java.io.File
 import java.io.FileOutputStream
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import coil.compose.rememberAsyncImagePainter
+import com.example.thecontactsapp.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +60,124 @@ class MainActivity : ComponentActivity() {
 
         }
     }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddContactScreen(viewModel: ContactViewModel, navController: NavController) {
+    val context = LocalContext.current.applicationContext
+
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    var name by remember { mutableStateOf("") }
+    var phone by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+
+    var launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+        imageUri = uri
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                modifier = Modifier.height(48.dp),
+                title = {
+                    Box (
+                        modifier = Modifier.fillMaxHeight().wrapContentHeight(Alignment.CenterVertically),
+                    ) {
+                        Text(
+                            text = "Add Contact",
+                            fontSize = 18.sp,
+                        )
+                    }
+                },
+                navigationIcon = {
+                    IconButton(onClick = { Toast.makeText(context, "Add Contact", Toast.LENGTH_SHORT).show() }) {
+                        Icon(painter= painterResource(id = R.drawable.addcontact), contentDescription = null)
+                    }
+                }, colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = PurpleMain,
+                    titleContentColor = Color.White,
+                    navigationIconContentColor = Color.White
+                )
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(paddingValues).padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            imageUri?.let { uri ->
+                Image(
+                    painter = rememberAsyncImagePainter(uri),
+                    contentDescription = null,
+                    modifier = Modifier.size(128.dp).clip(CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Spacer(modifier = Modifier.height(12.dp))
+            Button(onClick = {launcher.launch("image/*") },
+                colors = ButtonDefaults.buttonColors(PurpleMain)) {
+                Text("Add Image")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Name") },
+                modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(8.dp)),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White,
+                    focusedTextColor = Color.Black,
+                    unfocusedTextColor = Color.Black
+                )
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = phone,
+                onValueChange = { phone = it },
+                label = { Text("Phone Number") },
+                modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(8.dp)),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White,
+                    focusedTextColor = Color.Black,
+                    unfocusedTextColor = Color.Black
+                )
+            )
+            TextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(8.dp)),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.White,
+                    focusedTextColor = Color.Black,
+                    unfocusedTextColor = Color.Black
+                )
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {
+                imageUri?.let {
+                    val path = copyURIToInternalStorage(it, context, "$name.jpg")
+                    path?.let { iPath ->
+                        viewModel.add(iPath, name, phone, email)
+                        navController.navigate("contactList"){
+                            popUpTo(0)
+                        }
+                    }
+                }
+            }, colors = ButtonDefaults.buttonColors(PurpleMain)) {
+                Text("Add Contact")
+            }
+        }
+    }
+
 }
 
 fun copyURIToInternalStorage(uri: Uri, context: Context, fileName: String): String? {
