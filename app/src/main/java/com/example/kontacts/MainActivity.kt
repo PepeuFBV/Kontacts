c733b18f3355919578ce4a888bee6eaf16fde0d1
Pedro <pedrofigueiraboaviagem@gmail.com>
2024-10-13 14:40:43 -0300

    main fixes

--- app/src/main/java/com/example/kontacts/MainActivity.kt
+++ app/src/main/java/com/example/kontacts/MainActivity.kt
@@ -3,21 +3,17 @@ package com.example.kontacts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.kontacts.ui.theme.KontactsTheme
+import androidx.activity.viewModels
+import androidx.room.Room
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        
+        val database = Room.databaseBuilder(applicationContext, ContactDB::class.java, "contacts_db").build()
+        val repository = ContactRepository(database.contactDao())
+        val viewModel: ContactViewModel by viewModels { ContactViewModelFactory(repository) }
+
         setContent {
 
         }
